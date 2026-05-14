03951703e81213fc201fa8678e1d3962eb55088a
geez14 <geez14@noreply>
2026-05-14 14:39:36 +0530

    Add: speed insight functionality

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import Head from 'next/head';
 import ErrorBoundary from '@/components/ErrorBoundary';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }) {
   const siteTitle = 'VidMeet — quiet, fast 2-person video.';
