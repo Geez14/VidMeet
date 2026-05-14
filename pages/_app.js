0e80a4c5047a9f8e58a4c3b8c707086556591e01
geez14 <geez14@noreply>
2026-05-14 15:08:33 +0530

    Refactoring

--- pages/_app.js
+++ pages/_app.js
@@ -36,6 +36,7 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ErrorBoundary>
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
